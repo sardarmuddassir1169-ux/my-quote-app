c6dee52846b043476b3c336c4b0cbba3649b26e3
sardarmuddassir1169-ux <sardarmuddassir1169@gmail.com>
2026-06-26 21:44:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-const quotes = [
-    "Believe in yourself!",
-    "Every day is a new beginning.",
-    "You are capable of amazing things.",
-    "Small steps lead to big results."
-];
+const quoteText = document.querySelector(".quote"),
+quoteBtn = document.querySelector("#new-quote-btn"),
+authorName = document.querySelector(".name"),
+speechBtn = document.querySelector(".speech"),
+copyBtn = document.querySelector(".copy"),
+twitterBtn = document.querySelector(".twitter");
 
-const btn = document.getElementById("btn");
-const text = document.getElementById("quote");
+// Function to get a random quote from the Internet
+function randomQuote(){
+    quoteBtn.innerText = "Loading...";
+    // Fetching data from a free API
+    fetch("https://api.quotable.io/random").then(res => res.json()).then(result => {
+        quoteText.innerText = result.content;
+        authorName.innerText = result.author;
+        quoteBtn.innerText = "New Quote";
+    });
+}
 
-btn.addEventListener("click", () => {
-    const random = Math.floor(Math.random() * quotes.length);
-    text.innerText = quotes[random];
+// Speak the quote
+speechBtn.addEventListener("click", ()=>{
+    let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
+    speechSynthesis.speak(utterance);
 });
+
+// Copy the quote
+copyBtn.addEventListener("click", ()=>{
+    navigator.clipboard.writeText(quoteText.innerText);
+    alert("Quote copied to clipboard!");
+});
+
+// Post to Twitter (X)
+twitterBtn.addEventListener("click", ()=>{
+    let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+    window.open(tweetUrl, "_blank");
+});
+
+quoteBtn.addEventListener("click", randomQuote);
